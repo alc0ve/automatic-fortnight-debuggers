5dd41df9ca1d81a3ea46594e1e7965a2ee9fb0ef
Christina Hall <cmcclai26@gmail.com>
2023-04-07 16:17:33 -0400

    got basic routes going in template handlebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,45 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const description = document.querySelector("#idea-desc").value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+  if (description) {
+    const response = await fetch(`/api/ideas`, {
+      method: "POST",
+      body: JSON.stringify({ description }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to create project');
+      alert("Failed to create your idea!");
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
+    const response = await fetch(`/api/ideas/${id}`, {
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to delete project');
+      alert("Failed to delete idea.");
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+  .querySelector(".new-idea-form")
+  .addEventListener("submit", newFormHandler);
 
 document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".idea-list")
+  .addEventListener("click", delButtonHandler);
