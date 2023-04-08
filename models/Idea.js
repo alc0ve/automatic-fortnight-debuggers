0e0b6efa53a5b84a7c95f30ca61c1d0aa680afaf
Christina Hall <cmcclai26@gmail.com>
2023-04-08 16:29:33 -0400

    capitalized D in description in profile and added date to render for created date

--- models/Idea.js
+++ models/Idea.js
@@ -15,13 +15,18 @@ Idea.init(
     description: {
       type: DataTypes.STRING,
     },
-    // user_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "user",
-    //     key: "id",
-    //   },
-    // },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
