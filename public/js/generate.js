7c10aec1ff361e442691d2a0d34d0b70413dc9d8
Christina Hall <cmcclai26@gmail.com>
2023-04-11 17:42:17 -0400

    v3

--- public/js/generate.js
+++ public/js/generate.js
@@ -1,45 +1,43 @@
 console.log("test");
 const apps = [
+  "Youtube",
+  "TikTok",
+  "Uber",
+  "Twitter",
+  "Instagram",
+  "Facebook",
+  "To-do list",
+  "News app",
+  "Spotify",
+  "Pinterest",
+  "Snapchat",
+  "Dating app",
+  "WhatsApp",
+  "Blog",
+  "Search engine",
+  "Maps",
+  "Airbnb",
+  "Instacart",
+];
 
-    'Youtube',
-    'TikTok',
-    'Uber',
-    'Twitter',
-    'Instagram',
-    'Facebook',
-    'To-do list',
-    'News app',
-    'Spotify',
-    'Pinterest',
-    'Snapchat',
-    'Dating app',
-    'WhatsApp',
-    'Blog',
-    'Search engine',
-    'Maps',
-    'Airbnb',
-    'Instacart'
-    
-    ]
-    
 const descriptions = [
-    'Programmers',
-    'Dogs',
-    'Dancers',
-    'Dog owners',
-    'Wedding planners',
-    'Athletes',
-    'Teachers',
-    'Doctors',
-    'Scientists',
-    'the elderly',
-    'Social workers',
-    'Musicians',
-    'Mechanics',
-    'Foodies',
-    'Influencers'
-    ]
-    
+  "Programmers",
+  "Dogs",
+  "Dancers",
+  "Dog owners",
+  "Wedding planners",
+  "Athletes",
+  "Teachers",
+  "Doctors",
+  "Scientists",
+  "the elderly",
+  "Social workers",
+  "Musicians",
+  "Mechanics",
+  "Foodies",
+  "Influencers",
+];
+
 const generateButton = document.getElementById("generateButton");
 console.log(generateButton);
 generateButton.addEventListener("click", () => {
@@ -71,13 +69,18 @@ saveIdea.addEventListener("click", async () => {
     body: JSON.stringify({ description }),
     headers: { "Content-Type": "application/json" },
   });
+  if (response.ok) {
+    document.location.replace("/profile");
+  } else {
+    alert("Failed to create your idea!");
+  }
   console.log(response);
 });
 
-const saveBtn = document.getElementById("saveIdea");
-saveBtn.addEventListener("click", async () => {
-  const response = await fetch("/api/ideas", {
-    method: "GET",
-  });
-  console.log("got ideas", response);
-});
+// const saveBtn = document.getElementById("saveIdea");
+// saveBtn.addEventListener("click", async () => {
+//   const response = await fetch("/api/ideas", {
+//     method: "GET",
+//   });
+//   console.log("got ideas", response);
+// });
