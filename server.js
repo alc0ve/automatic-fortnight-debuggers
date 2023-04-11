28522564f0a0ad30687b021f88c9e9ea7028a832
Christina Hall <cmcclai26@gmail.com>
2023-04-11 19:03:06 -0400

    v6

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
